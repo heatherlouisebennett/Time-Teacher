d351b631b803159e2d4185106961db46098e1122
Myles Bennett <myles.bennett@consultant.com>
2017-02-09 23:30:26 +0000

    scalable sroke width

--- timeteachermodule/src/main/java/com/aimicor/timeteachermodule/CircleView.java
+++ timeteachermodule/src/main/java/com/aimicor/timeteachermodule/CircleView.java
@@ -2,15 +2,16 @@ package com.aimicor.timeteachermodule;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 
+import static android.graphics.Color.BLACK;
+import static android.graphics.Paint.Style.STROKE;
+
 public class CircleView extends SquareView {
 
-    private static final int STROKE_WIDTH = 10;
-    private final Paint mPaint = getPaint();
+    private static final float STROKE_WIDTH_FACTOR = 1f;
 
     public CircleView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -18,17 +19,18 @@ public class CircleView extends SquareView {
 
     @Override
     protected void draw(Canvas canvas, int diameter) {
-        int radius = diameter / 2;
-        canvas.drawCircle(radius, radius, radius - STROKE_WIDTH / 2, mPaint);
+        float radius = diameter / 2f;
+        float strokeWidth = STROKE_WIDTH_FACTOR * diameter / 100f;
+        canvas.drawCircle(radius, radius, radius - strokeWidth / 2f, getPaint(strokeWidth));
     }
 
     @NonNull
-    private Paint getPaint() {
+    private Paint getPaint(float strokeWidth) {
         Paint paint = new Paint();
         paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(STROKE_WIDTH);
-        paint.setColor(Color.BLACK);
+        paint.setStyle(STROKE);
+        paint.setStrokeWidth(strokeWidth);
+        paint.setColor(BLACK);
         return paint;
     }
 }
