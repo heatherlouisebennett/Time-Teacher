b9c6fb891c1ac96cd0ee15a3694df8edd13ba10c
Myles Bennett <myles.bennett@consultant.com>
2017-02-21 13:35:43 +0000

    wip

--- timeteachermodule/src/main/java/com/aimicor/timeteachermodule/MinuteViewVerticalMoverImpl.java
+++ timeteachermodule/src/main/java/com/aimicor/timeteachermodule/MinuteViewVerticalMoverImpl.java
@@ -1,5 +1,6 @@
 package com.aimicor.timeteachermodule;
 
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
@@ -55,6 +56,7 @@ public class MinuteViewVerticalMoverImpl implements View.OnTouchListener, Animat
 
     @Override
     public void onAnimationEnd(Animation animation) {
+        Log.i("mydebug", "onAnimationEnd");
         if (mMinuteView.getY() < mOpenPosition / 2) {
             mMinuteView.setY(0);
         } else {
